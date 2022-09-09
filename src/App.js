e7cd714b909c3ce8de219575246cd8f248cda3d8
Lucasabdala94 <83674006+Lucasabdala94@users.noreply.github.com>
2022-09-09 05:42:14 -0300

    navbar terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import App from './app';
+import Home from './pages/Home';
+import Contacto from './pages/Contacto';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <header>
+        <h1>Titulo Header APP</h1>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/contacto">Contacto</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <hr />
+      <hr />
+      <hr />
+
+      <Routes>
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }

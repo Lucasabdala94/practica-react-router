4bd6f8ec6c56a224bb8ebf170c8bb7b685a3ed8c
Lucasabdala94 <83674006+Lucasabdala94@users.noreply.github.com>
2022-09-09 05:48:34 -0300

    tutorial 34min

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import App from './app';
 import Home from './pages/Home';
 import Contacto from './pages/Contacto';
+import Tacos from './components/Tacos';
 
 export default function App() {
   return (
@@ -28,6 +29,7 @@ export default function App() {
       <Routes>
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/" element={<Home />} />
+        <Route path="/tacos" element={<Tacos />} />
       </Routes>
     </div>
   );
---
+++ src/components/Tacos.js
@@ -0,0 +1,7 @@
+export default function Tacos() {
+  return (
+    <div>
+      <h1>Paginas de tacos</h1>
+    </div>
+  );
+}
